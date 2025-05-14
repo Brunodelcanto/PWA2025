ea1f0e9505829f8ae681a1ca6c27cf7d5d781d26
Bruno Del Canto <delcantobruno5@gmail.com>
2025-05-13 21:02:11 -0300

    add CRUD

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -41,5 +41,105 @@ const createUser = async (req, res)=>{
     }
 }
 
+// Esta funcion recibe todos los usuarios y los devuelve en formato json
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            message: "Fetched successfully",
+            data: users,
+            error: false,
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+};
+
+// Esta funcion recibe un id y devuelve el usuario correspondiente
+const getUserById = async (req, res) => {
+    try {
+        // le enviamos el id de usuario que queremos buscar
+        const { id } = req.params;
+        // buscamos el usuario filtrandolo por el id
+        // esto nos va a devoler un objetoo con la informacion del usuario
+        const user = await User.findById(id);
+        // si el usuario no existe, devolvemos un error
+        if (!user){
+            return res.status(404).json({
+                message: "User not found",
+                error: true,
+            });
+        }
+        // si el usuario existe, devolvemos la informacion del usuario
+        res.status(200).json({
+            message: "Fetched successfully",
+            data: user,
+            error: false,  
+        })
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+};
+
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(
+            // como primer arumento le pasamos el id del usuario que queremos actualizar
+            id,
+            {
+            // como segundo argumento le pasamos la informacion que queremos actualizar
+                $set: req.body,
+            },
+            // con el new devolvemos el usuario actualizado
+            { new: true }
+        );
+        if (!user){
+            return res.status(404).json({
+                message: "User not found",
+                error: true,
+            });
+        }
+        res.status(200).json({
+            message: "Update successfully",
+            data: user,
+            error: false,
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+}
+
+const deleteUser = async (req, res)=>{
+    try {
+        // le pasamos el id del usuario que queremos eliminar
+        const { id } = req.params;
+        // y lo eliminamos de la base de datos
+        const user = await User.findByIdAndDelete(id);
+        if (!user){
+            return res.status(404).json({
+                message: "User not found",
+                error: true,
+            });
+        }
+        res.status(200).json({
+            message: "Deletes successfully",
+            error: false,
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+};
+
+
 // exportamos la funcion getUser para poder usarla en otros archivos 
-export { getUser, createUser };
+export { getUser, createUser, getUserById, getUsers, updateUser, deleteUser };
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,15 +1,23 @@
 import express from "express";
 // importamos el controlador de usuarios
 // el controlador es el encargado de recibir la peticion y devolver la respuesta
-import { getUser, createUser } from "../../controllers/users/index.js";
+import { getUser, createUser, getUserById, updateUser, getUsers, deleteUser } from "../../controllers/users/index.js";
 
 // guardamos en esta variable para poder usar el routeo de express
 const router = express.Router();
 
 // esta funcion recibe el objeto router y le asigna la ruta /users
 // y le asigna el controlador getUser
-router.get ("/", getUser);
+
+// metodo get para conseguir
+router.get ("/", getUsers);
+// metodo post para crear
 router.post ("/", createUser);
+router.get ("/:id", getUserById);
+// metodo put para actualizar 
+router.put ("/:id", updateUser);
+// metodo delete para eliminar
+router.delete ("/:id", deleteUser);
 
 export default router;
 // exportamos el router para poder usarlo en otros archivos
